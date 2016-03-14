d22d99e202ac73f2f9394ae19287bae992cd5524
lxmiccio <miccio.alex@gmail.com>
2016-03-14 14:54:52 +0100

    Created composite key in UserLike

--- fiabeonline/src/AppBundle/Entity/UserLike.php
+++ fiabeonline/src/AppBundle/Entity/UserLike.php
@@ -15,32 +15,39 @@ class UserLike
     /**
      * @var int
      *
-     * @ORM\Column(name="id", type="integer")
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
-
-    /**
      * @ORM\ManyToOne(targetEntity="User", inversedBy="likes")
      * @ORM\JoinColumn(name="User_id", referencedColumnName="id")
      */
     protected $userId;
 
     /**
+     * @var int
+     *
+     * @ORM\Id
      * @ORM\ManyToOne(targetEntity="Tale", inversedBy="likes")
      * @ORM\JoinColumn(name="Tale_id", referencedColumnName="id")
      */
     protected $taleId;
 
     /**
-     * Get id
+     * Get userId
      *
-     * @return integer
+     * @return \AppBundle\Entity\User
      */
-    public function getId()
+    public function getUserId()
     {
-        return $this->id;
+        return $this->userId;
+    }
+
+    /**
+     * Get taleId
+     *
+     * @return \AppBundle\Entity\Tale
+     */
+    public function getTaleId()
+    {
+        return $this->taleId;
     }
 
     /**
@@ -49,21 +56,11 @@ class UserLike
      * @param \AppBundle\Entity\User $userId
      * @return UserLike
      */
-    public function setUserId(\AppBundle\Entity\User $userId = null)
+    public function setUserId(\AppBundle\Entity\User $userId)
     {
         $this->userId = $userId;
 
         return $this;
-    }
-
-    /**
-     * Get userId
-     *
-     * @return \AppBundle\Entity\User 
-     */
-    public function getUserId()
-    {
-        return $this->userId;
     }
 
     /**
@@ -72,20 +69,10 @@ class UserLike
      * @param \AppBundle\Entity\Tale $taleId
      * @return UserLike
      */
-    public function setTaleId(\AppBundle\Entity\Tale $taleId = null)
+    public function setTaleId(\AppBundle\Entity\Tale $taleId)
     {
         $this->taleId = $taleId;
 
         return $this;
-    }
-
-    /**
-     * Get taleId
-     *
-     * @return \AppBundle\Entity\Tale 
-     */
-    public function getTaleId()
-    {
-        return $this->taleId;
     }
 }

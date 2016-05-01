a99ec71515cfe18197a99a99da77b2b4bf96b503
lxmiccio <miccio.alex@gmail.com>
2016-05-01 14:23:11 +0200

    Rimosso Partecipate.php~

--- fiabeonline/src/AppBundle/Entity/ActionPropp.php
+++ fiabeonline/src/AppBundle/Entity/ActionPropp.php
@@ -35,4 +35,73 @@ class ActionPropp
      * @ORM\Column(name="score", type="integer", options={"default" = 5})
      */
     private $score;
+
+    /**
+     * Set score
+     *
+     * @param integer $score
+     * @return ActionPropp
+     */
+    public function setScore($score)
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+
+    /**
+     * Get score
+     *
+     * @return integer 
+     */
+    public function getScore()
+    {
+        return $this->score;
+    }
+
+    /**
+     * Set card
+     *
+     * @param \AppBundle\Entity\Card $card
+     * @return ActionPropp
+     */
+    public function setCard(\AppBundle\Entity\Card $card)
+    {
+        $this->card = $card;
+
+        return $this;
+    }
+
+    /**
+     * Get card
+     *
+     * @return \AppBundle\Entity\Card 
+     */
+    public function getCard()
+    {
+        return $this->card;
+    }
+
+    /**
+     * Set sequenceType
+     *
+     * @param \AppBundle\Entity\SequenceType $sequenceType
+     * @return ActionPropp
+     */
+    public function setSequenceType(\AppBundle\Entity\SequenceType $sequenceType)
+    {
+        $this->sequenceType = $sequenceType;
+
+        return $this;
+    }
+
+    /**
+     * Get sequenceType
+     *
+     * @return \AppBundle\Entity\SequenceType 
+     */
+    public function getSequenceType()
+    {
+        return $this->sequenceType;
+    }
 }
